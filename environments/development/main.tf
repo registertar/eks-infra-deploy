fa6717fc809504c18c7dab90b45578d04aafeba3
Zoltan Tar <zoltan_tar2@epam.com>
2023-08-16 16:39:24 +0200

    add kms cmk

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -1,7 +1,13 @@
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "test-bucket-08162023-a"
+resource "aws_kms_key" "master_cmk" {
+  description             = "Master KMS key"
+  deletion_window_in_days = 7
+}
 
-  tags = {
-    Environment = "development"
-  }
+resource "aws_kms_alias" "master_cmk" {
+  name          = "alias/eks"
+  target_key_id = aws_kms_key.master_cmk.key_id
+}
+
+output "MASTER_ARN" {
+  value = aws_kms_alias.master_cmk.arn
 }
